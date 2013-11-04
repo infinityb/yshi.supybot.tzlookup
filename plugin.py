4b5070fb1e68a07712b1567e7811a73db63e117b
Stacey Ell <stacey.ell@gmail.com>
2013-11-04 15:48:47 -0600

    Iniital commit

---
+++ plugin.py
@@ -0,0 +1,67 @@
+###
+# Copyright (c) 2013, Stacey Ell
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+import datetime
+
+#import supybot.utils as utils
+from supybot.commands import wrap
+#import supybot.plugins as plugins
+#import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+try:
+    from supybot.i18n import PluginInternationalization
+    _ = PluginInternationalization('PrinterStatus')
+except:
+    # Placeholder that allows to run the plugin on a bot
+    # without the i18n module
+    _ = lambda x: x
+
+import pytz
+
+
+class TZLookup(callbacks.Plugin):
+    """Add the help for "@plugin help TZLookup" here
+    This should describe *how* to use this plugin."""
+    def tz(self, irc, msg, args, tzname):
+        """<posix timezone name>
+
+        Show the current time in some timezone"""
+        tz = pytz.timezone(tzname)
+        curtime = pytz.utc.localize(datetime.datetime.utcnow())
+        fmttime = curtime.astimezone(tz).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        # import pdb; pdb.set_trace()
+        irc.reply("{0.nick}: fmttime".format(msg, fmttime))
+
+    tz = wrap(tz, ['text'])
+
+
+Class = TZLookup
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
